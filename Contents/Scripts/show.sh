7c07715c2846e329dd095297502aeb63307d784c
Quang-Linh LE <linktohack@gmail.com>
2016-02-11 00:23:10 +0100

    Reduce sleep time...

--- Contents/Scripts/show.sh
+++ Contents/Scripts/show.sh
@@ -10,8 +10,8 @@ URL=$(echo "$INFO" | grep '^URL: ' | sed -e 's/URL: //')
 PW=$(echo "$INFO" | grep '^Password: ' | sed -e 's/Password: //')
 UN=$(echo "$INFO" | grep '^Username: ' | sed -e 's/Username: //')
 
-[ -z $PW ] || (echo "$PW" | pbcopy; sleep 1)
-[ -z $UN ] || (echo "$UN" | pbcopy; sleep 1)
+[ -z $PW ] || (echo "$PW" | pbcopy; sleep 0.2)
+[ -z $UN ] || (echo "$UN" | pbcopy)
 
 echo "$URL"
 echo "$INFO"
